aa56000df72558d92d48aa28508312c7264aa462
sshvch <mka28112005@gmail.com>
2026-04-29 21:21:09 +0500

    Update MessageInput.jsx

--- src/app/[userId]/[chatId]/ChatComponents/MessageInput.jsx
+++ src/app/[userId]/[chatId]/ChatComponents/MessageInput.jsx
@@ -12,7 +12,7 @@ export default function MessageInput({ senderId, chatId, onSendMessage }) {
         setIsSending(true);
         
         try {
-const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/messages/send`, {
+const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/messages/send`, {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({
